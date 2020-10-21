2b2d63aa6fe2c1f978837511b5fd50a85a29410c
jakkapat <jakkapat.b@blockfint.com>
2020-10-21 15:53:44 +0700

    🚧  creating card

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import Link from 'next/link'
 const Card = styled.div`
   border-radius: 1rem;
   box-shadow: 0 8px 16px 0 #f0f3fa;
   border: solid 1px #fafafa;
   background-color: var(--blockfint-grey-white-2);
-  display:grid;
-  grid-template-columns:1fr 1fr;
-}
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 35.625rem;
+  height: 12.75rem;
+  overflow: hidden;
 `
+
 const Image = styled.img`
   background: blue;
+  width: 100%;
+  height: 12.75rem;
 `
-export const PostCard: React.FC = () => {
+
+const Content = styled.div`
+  margin: 1.5rem;
+`
+
+interface Props {
+  title?: string
+  desc?: string
+  link?: string
+  imgSrc?: string
+}
+export const PostCard: React.FC<Props> = ({ title, desc, link = '/', imgSrc }) => {
   return (
     <Card>
-      <Image />
+      <Image src={imgSrc} />
+      <Content>
+        <span>{title}</span>
+        <span>{desc}</span>
+        <Link href={link} passHref>
+          <a>See more</a>
+        </Link>
+      </Content>
     </Card>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { ReactComponent as Logo } from 'assets/logos/blockfint-small.svg'
 import { SecondaryButton, PrimaryButton } from 'components/Buttons'
+import { PostCard } from 'components/PostCard'
+import React from 'react'
 export default function Home() {
   return (
     <div style={{ margin: '2rem' }}>
       <SecondaryButton>SSS</SecondaryButton>
       <PrimaryButton logo={<Logo style={{ fontSize: '1.5rem' }} />}>Label</PrimaryButton>
+      <PostCard title="Gideon" desc="Energy Trading Without Barriers" />
     </div>
   )
 }
