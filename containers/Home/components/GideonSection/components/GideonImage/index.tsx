a3f747556d679c2f2d372ee227df052df5f6b50b
jakkapat <jakkapat.b@blockfint.com>
2020-10-22 16:48:30 +0700

    🐛  fix animation

--- containers/Home/components/GideonSection/components/GideonImage/index.tsx
+++ containers/Home/components/GideonSection/components/GideonImage/index.tsx
@@ -5,6 +5,7 @@ import Oval from './components/Oval'
 import Triangle from './components/Triangle'
 import { motion } from 'framer-motion'
 import { BREAKPOINT } from 'assets/globalStyle'
+import { useInView } from 'react-intersection-observer'
 const Image = styled.img`
   max-width: 100vw;
 `
@@ -27,27 +28,38 @@ const Wrapper = styled(motion.div)`
   position: absolute;
 `
 export const GideonImage: React.FC = () => {
+  const { ref, inView } = useInView()
   return (
-    <Container>
+    <Container ref={ref}>
       <Image src="images/gideon-preview.png" />
-      <IconWrapper bottom={0} left="50%">
-        <Thunder color="#7CD55E" />
-      </IconWrapper>
-      <IconWrapper top={'10%'} left="54%" size="2em">
-        <Oval color="#00C1FF" />
-      </IconWrapper>
-      <IconWrapper top={'40%'} left="82.5%" size="1.8em">
-        <Oval color="#00C1FF" />
-      </IconWrapper>
-      <IconWrapper bottom={0} right="5%">
-        <Triangle color="#FF7278" />
-      </IconWrapper>
-      <IconWrapper bottom={'10%'} left="17.5%" size="1.2em">
-        <Oval color="#FF7278" />
-      </IconWrapper>
-      <IconWrapper top={'5%'} left="5%" size="3.5em">
-        <Oval color="#FF7278" />
-      </IconWrapper>
+      {inView && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            delay: 0.5
+          }}
+        >
+          <IconWrapper bottom={0} left="50%">
+            <Thunder color="#7CD55E" />
+          </IconWrapper>
+          <IconWrapper top={'10%'} left="54%" size="2em">
+            <Oval color="#00C1FF" />
+          </IconWrapper>
+          <IconWrapper top={'40%'} left="82.5%" size="1.8em">
+            <Oval color="#00C1FF" />
+          </IconWrapper>
+          <IconWrapper bottom={0} right="5%">
+            <Triangle color="#FF7278" />
+          </IconWrapper>
+          <IconWrapper bottom={'10%'} left="17.5%" size="1.2em">
+            <Oval color="#FF7278" />
+          </IconWrapper>
+          <IconWrapper top={'5%'} left="5%" size="3.5em">
+            <Oval color="#FF7278" />
+          </IconWrapper>
+        </motion.div>
+      )}
     </Container>
   )
 }
