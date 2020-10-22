3337bd3a6d62122cdd523abc6c2a6917024ad2a1
jakkapat <jakkapat.b@blockfint.com>
2020-10-22 17:42:36 +0700

    💄 Create product banner and product sections

---
+++ containers/Home/components/04-NeoBankSection/index.tsx
@@ -0,0 +1,41 @@
+import { PrimaryButton } from 'components/Buttons'
+import React from 'react'
+import styled from 'styled-components'
+import { ReactComponent as NeoBankSmall } from 'assets/logos/neobank-small.svg'
+import { Container } from '@material-ui/core'
+import { BREAKPOINT } from 'assets/globalStyle'
+import { GideonImage } from './components/GideonImage'
+const Content = styled.div`
+  margin: 6.25rem 0;
+  @media ${BREAKPOINT.tablet} {
+    margin: 10.25rem 0 8.25rem;
+  }
+  text-align: center;
+`
+const Title = styled.h5`
+  line-height: 1.46;
+`
+const Desc = styled.p`
+  max-width: 35rem;
+  margin: 0.5rem auto 2.5rem;
+  line-height: 1.88;
+`
+
+export const NeoBankSection: React.FC = () => {
+  return (
+    <Container maxWidth="lg">
+      <Content>
+        <GideonImage />
+        <Title style={{ color: 'var(--primary)' }}>Neo Bank</Title>
+        <Title>The New Era of Banking Services</Title>
+        <Desc>
+          Banking experience redefined! A digital banking platform simplifying access to capital and incorporating
+          innovative ways to grow your money.
+        </Desc>
+        <PrimaryButton logo={<NeoBankSmall />} background={'var(--gradient-neo-bank)'}>
+          Read more
+        </PrimaryButton>
+      </Content>
+    </Container>
+  )
+}
--- containers/Home/index.tsx
+++ containers/Home/index.tsx
@@ -1,13 +1,21 @@
 import { ContactBanner } from 'components/ContactBanner'
 import React from 'react'
-import { GideonSection } from './components/GideonSection'
-import { Hero } from './components/Hero'
+import { Hero } from './components/00-Hero'
+import { GideonSection } from './components/01-GideonSection'
+import { ThinkerSection } from './components/02-ThinkerSection'
+import { PalmSection } from './components/03-PalmSection'
+import { NeoBankSection } from './components/04-NeoBankSection'
+import { ProductsBanner } from './components/05-ProductsBanner'
 
 export const Home: React.FC = () => {
   return (
     <>
       <Hero />
       <GideonSection />
+      <ThinkerSection />
+      <PalmSection />
+      <NeoBankSection />
+      <ProductsBanner />
       <ContactBanner />
     </>
   )
