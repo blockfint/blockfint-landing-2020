433194649e03631604c2aa8317d181e29520e5bf
Thanakrit Buranakarn <thanakrit@Thanakrits-MacBook-Pro.local>
2020-10-29 16:08:44 +0700

    :lipstick: responsive footer and add social icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
+import { Layout } from 'components/layouts'
 import { Home } from 'containers/Home'
 import { NextPage } from 'next'
 import React from 'react'
-import { Navbar } from '../components/layouts/Navbar'
 
 const Homepage: NextPage = () => {
   return (
-    <div>
-      <Navbar transparent={false} />
+    <Layout>
       <Home />
-    </div>
+    </Layout>
   )
 }
 
