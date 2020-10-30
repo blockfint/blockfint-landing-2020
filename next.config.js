bf750682b51698a28895c117b913bf7caeaed9e9
jakkapat <jakkapat.b@blockfint.com>
2020-10-30 16:32:47 +0700

    remove github page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH + '/' : '/',
-  dynamicAssetPrefix: true,
   images: {
     deviceSizes: [320, 420, 768, 1024, 1200]
   },
