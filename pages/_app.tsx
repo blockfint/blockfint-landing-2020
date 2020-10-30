44620b3482714467ffa5f1da0d5fbc8bdcfa71db
jakkapat <jakkapat.b@blockfint.com>
2020-10-30 16:41:10 +0700

    add remove ssr jss injected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,39 @@ import { AllStyleProvider } from 'assets/globalStyle'
 import React from 'react'
 import { ContactDialog } from 'components/ContactDialog'
 import { DefaultSeo } from 'next-seo'
+import App from 'next/app'
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
-          rel="stylesheet"
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <DefaultSeo
+          title="Blockfint"
+          titleTemplate="%s | Make Future Innovations Happen Today"
+          description="Make Future Innovations Happen Today"
         />
-      </Head>
-      <DefaultSeo
-        title="Blockfint"
-        titleTemplate="%s | Make Future Innovations Happen Today"
-        description="Make Future Innovations Happen Today"
-      />
-      <AllStyleProvider>
-        <ContactDialog>
-          <Component {...pageProps} />
-        </ContactDialog>
-      </AllStyleProvider>
-    </>
-  )
+        <AllStyleProvider>
+          <ContactDialog>
+            <Component {...pageProps} />
+          </ContactDialog>
+        </AllStyleProvider>
+      </>
+    )
+  }
 }
 
 export default MyApp
