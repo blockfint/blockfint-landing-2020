adb661335b6c1a360acdae6dcd8422a2f42a3bba
Thanakrit Buranakarn <thanakrit@Thanakrits-MacBook-Pro.local>
2020-11-03 17:13:18 +0700

    :lipstick: fix padding product detail

--- components/productDetail.tsx
+++ components/productDetail.tsx
@@ -5,8 +5,14 @@ import { BREAKPOINT } from 'assets/globalStyle'
 import { Container } from '@material-ui/core'
 
 const Content = styled.div`
-  margin: 6.25rem 0 2rem;
   padding: 0 0.5rem;
+  margin: 2.5rem 0;
+  @media ${BREAKPOINT.tablet} {
+    margin: 3.75rem 0;
+  }
+  @media ${BREAKPOINT.desktop} {
+    margin: 6.25rem 0;
+  }
 `
 const BlueTitle = styled.h2`
   max-width: 60rem;
