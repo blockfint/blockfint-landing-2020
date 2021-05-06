2cf38bab8fcde4ecc8c738179062b9352ab76ed6
jakkapat <max.jakkapat@gmail.com>
2021-05-06 12:49:07 +0700

    🐛  fix ssr false

--- apps/website/pages/poc.tsx
+++ apps/website/pages/poc.tsx
@@ -1,33 +1,11 @@
 import { NextPage } from 'next'
-import React, { useMemo } from 'react'
-import { PrimaryButton } from '../components/Buttons'
+import React from 'react'
+import { ShareButton } from '../components/ShareButton'
 
 const POC: NextPage = () => {
-  const isSupportShare = useMemo(() => {
-    if (navigator && navigator.share) {
-      return true
-    }
-    return false
-  }, [])
-  const handleShare = async () => {
-    console.log('hello')
-    if (navigator.share) {
-      try {
-        await navigator.share({
-          title: 'Title share API POC',
-          text: 'Text share API POC',
-          url: 'https://blockfint.com'
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      alert('Not Support')
-    }
-  }
   return (
     <div>
-      {isSupportShare ? <PrimaryButton onClick={handleShare}>Share</PrimaryButton> : <h1>Not Support Share API</h1>}
+      <ShareButton />
     </div>
   )
 }
