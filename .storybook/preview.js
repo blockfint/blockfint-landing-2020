f1d85420e2e5b5102c8c8ac5e2da90c1d9942a83
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-07 11:35:28 +0700

    🐛 fix path

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -139,13 +139,6 @@ export const decorators = [
 export const parameters = {
   layout: 'center',
   actions: { argTypesRegex: '^on[A-Z].*' },
-  // Storybook a11y addon configuration
-  a11y: {
-    // the target DOM element
-    element: '#root',
-    // sets the execution mode for the addon
-    manual: false
-  },
   viewport: {
     viewports: {
       mobile: {
