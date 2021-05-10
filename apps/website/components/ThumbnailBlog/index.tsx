34d8a03a7c039b0171b7c13a32de3801ce7c6fcc
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-10 18:58:18 +0700

    📝 add story mobile, tablet , desktop

--- apps/website/components/ThumbnailBlog/index.tsx
+++ apps/website/components/ThumbnailBlog/index.tsx
@@ -1,14 +1,62 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import dayjs from 'dayjs'
-const ImageWrapper = styled.div<{ imgSrc: string }>`
-  width: 328px;
+import { BREAKPOINT } from '@blockfint/website/styles/globalStyle'
+const Wrapper = styled.div<{ size: 'M' | 'L' }>`
+  display: grid;
+  grid-gap: 1rem;
+  ${({ size }) => {
+    if (size === 'M') {
+      return css`
+        @media ${BREAKPOINT.tablet} {
+          grid-template-columns: 362px auto;
+        }
+        @media ${BREAKPOINT.desktop} {
+          grid-template-columns: 485px auto;
+        }
+      `
+    } else if (size === 'L') {
+      return css`
+        display: flex;
+        flex-direction: column-reverse;
+        grid-row-gap: 1.5rem;
+      `
+    }
+  }};
+`
+const ImageWrapper = styled.div<{ imgSrc: string; size: 'M' | 'L' }>`
   height: 172px;
   border-radius: 8px;
   background-color: #c4c4c4;
   background-image: url(${({ imgSrc }) => imgSrc});
   background-position: center;
   background-size: cover;
+  @media ${BREAKPOINT.desktop} {
+    height: 196px;
+  }
+  ${({ size }) => {
+    if (size === 'M') {
+      return css`
+        @media ${BREAKPOINT.tablet} {
+          height: 190px;
+          border-radius: 0;
+        }
+        @media ${BREAKPOINT.desktop} {
+          height: 254px;
+        }
+      `
+    } else if (size === 'L') {
+      return css`
+        border-radius: 0;
+        @media ${BREAKPOINT.tablet} {
+          height: 360px;
+        }
+        @media ${BREAKPOINT.desktop} {
+          height: 644px;
+        }
+      `
+    }
+  }}
 `
 const Tag = styled.div`
   width: 134px;
@@ -16,51 +64,112 @@ const Tag = styled.div`
   background-color: #e0e0e0;
   text-align: center;
   color: #4f4f4f;
+  grid-area: tag;
 `
 const DateTime = styled.div`
   display: grid;
   grid-template-columns: repeat(2, max-content);
   align-items: center;
   grid-column-gap: 0.5rem;
+  grid-area: datetime;
 `
 const Date = styled.h6`
   color: #bdbdbd;
 `
-const TextSection = styled.div`
-  display: gird;
-  grid-auto-rows: max-content;
+const TextSection = styled.div<{ size: 'M' | 'L' }>`
+  display: grid;
   grid-row-gap: 0.5rem;
-  margin-top: 1rem;
+  align-items: center;
+  grid-template-areas:
+    'tag'
+    'titletext'
+    'datetime';
+  ${({ size }) => {
+    if (size === 'M') {
+      return css`
+        @media ${BREAKPOINT.desktop} {
+          grid-template-areas:
+            'titletext titletext .'
+            'description description .'
+            'tag datetime datetime';
+          grid-column-gap: 0.5rem;
+        }
+      `
+    } else if (size === 'L') {
+      return css`
+        justify-items: center;
+        text-align: center;
+        h5 {
+          -webkit-line-clamp: 6;
+          max-height: 252px;
+        }
+        @media ${BREAKPOINT.tablet} {
+          justify-items: start;
+          text-align: start;
+          h5 {
+            -webkit-line-clamp: 3;
+            max-height: 138px;
+          }
+        }
+      `
+    }
+  }};
 `
 const Icon = styled.img`
   width: 16px;
   height: 16px;
 `
+const TitleText = styled.h5`
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  max-height: 114px;
+  overflow: hidden;
+  grid-area: titletext;
+`
+const Description = styled.p`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  max-height: 60px;
+  overflow: hidden;
+  grid-area: description;
+  display: none;
+  @media ${BREAKPOINT.desktop} {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+`
 interface Props {
   imgSrc?: string
   tag?: string
-  description?: string
+  title?: string
   publishDate?: Date
+  size?: 'M' | 'L'
+  description?: string
 }
 export const ThumbnailBlog: React.FC<Props> = ({
   imgSrc,
   tag = 'TECHNOLOGY',
-  description = 'No Description',
-  publishDate
+  title = 'No Title',
+  publishDate,
+  size,
+  description
 }) => {
   return (
-    <div>
-      <ImageWrapper imgSrc={imgSrc}></ImageWrapper>
-      <TextSection>
+    <Wrapper size={size}>
+      <ImageWrapper imgSrc={imgSrc} size={size} />
+      <TextSection size={size}>
         <Tag>
           <h6>{tag}</h6>
         </Tag>
-        <h5>{description}</h5>
+        <TitleText>{title}</TitleText>
+        {size === 'M' && <Description>{description}</Description>}
         <DateTime>
           <Icon src="/icons/clock.svg" alt="clock" />
           <Date>{dayjs(publishDate).format('DD MMMM YYYY')}</Date>
         </DateTime>
       </TextSection>
-    </div>
+    </Wrapper>
   )
 }
--- apps/website/components/ThumbnailBlog/thumbnail.stories.tsx
+++ apps/website/components/ThumbnailBlog/thumbnail.stories.tsx
@@ -5,10 +5,33 @@ export default {
 }
 
 const Template = (args) => (
-  <ThumbnailBlog
-    imgSrc="/images/kbtg.jpg"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
-  />
+  <>
+    <h1>Size S</h1>
+    <ThumbnailBlog
+      imgSrc="/images/kbtg.jpg"
+      title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor consectetur adipiscing elit,"
+      {...args}
+    />
+    <hr style={{ margin: '50px' }} />
+    <h1>Size M</h1>
+
+    <ThumbnailBlog
+      imgSrc="/images/kbtg.jpg"
+      title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor consectetur adipiscing elit,sed do eiusmod tempor consectetur adipiscing elit,"
+      size="M"
+      description="Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla"
+      {...args}
+    />
+    <hr style={{ margin: '50px' }} />
+    <h1>Size L</h1>
+
+    <ThumbnailBlog
+      imgSrc="/images/kbtg.jpg"
+      title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor consectetur adipiscing elit,"
+      size="L"
+      {...args}
+    />
+  </>
 )
 export const Mobile = Template.bind({})
 Mobile.parameters = {
@@ -16,3 +39,12 @@ Mobile.parameters = {
     defaultViewport: 'mobile'
   }
 }
+
+export const Tablet = Template.bind({})
+Tablet.parameters = {
+  viewport: {
+    defaultViewport: 'tablet'
+  }
+}
+
+export const Desktop = Template.bind({})
