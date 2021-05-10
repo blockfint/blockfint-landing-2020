e004483e2b6e1e7f1963045f0a586fb5821d20be
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-10 11:34:54 +0700

    ✨ Create thumbnail story

---
+++ apps/website/components/ThumbnailBlog/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ThumbnailBlog = () => {
+  return (
+    <div>
+      <img src="/kbtg.png" alt="thumbnail" />
+    </div>
+  )
+}
---
+++ apps/website/components/ThumbnailBlog/thumbnail.stories.tsx
@@ -0,0 +1,8 @@
+import { ThumbnailBlog } from '.'
+
+export default {
+  title: 'Components/ThumbnailBlog'
+}
+
+const Template = (args) => <ThumbnailBlog />
+export const Basic = Template.bind({})
