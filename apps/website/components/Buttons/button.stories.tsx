acdf5f7d064dfd7e8546a2236b974a62218e7b7f
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-11 11:31:09 +0700

    📝 add story button

---
+++ apps/website/components/Buttons/button.stories.tsx
@@ -0,0 +1,16 @@
+import { LinkButton } from './LinkButton'
+import { OrangeButton } from './OrangeButton'
+import { PrimaryButton } from './PrimaryButton'
+import { SecondaryButton } from './SecondaryButton'
+
+export default {
+  title: 'Components/Button'
+}
+
+export const Link = () => <LinkButton />
+
+export const Orange = () => <OrangeButton />
+
+export const Primary = () => <PrimaryButton />
+
+export const Secondary = () => <SecondaryButton />
