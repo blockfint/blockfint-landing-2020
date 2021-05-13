59dda8cba73f7258b432865fd737ca620c4e0295
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-13 12:32:27 +0700

    add getStaticProps

---
+++ apps/website/containers/Blog/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Blog: React.FC = () => {
+  return <div></div>
+}
--- apps/website/pages/blog/index.tsx
+++ apps/website/pages/blog/index.tsx
@@ -2,7 +2,10 @@ import { NextPage } from 'next'
 import React from 'react'
 import { typography } from '@blockfint/website/styles/typography'
 import { createGlobalStyle } from 'styled-components'
-
+import { Layout } from '@blockfint/website/components/layouts'
+import { Blog } from '@blockfint/website/containers/Blog'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import nextI18NextConfig from '../../next-i18next.config.js'
 const Global = createGlobalStyle`
 body{
   ${typography}
@@ -12,9 +15,18 @@ const BlogPage: NextPage = () => {
   return (
     <>
       <Global />
-      <div>BlogPage</div>
+      <Layout>
+        <Blog />
+      </Layout>
     </>
   )
 }
-
 export default BlogPage
+export const getStaticProps = async ({ locale }) => {
+  const result = await serverSideTranslations(locale, ['common', 'about'], nextI18NextConfig)
+  return {
+    props: {
+      ...result
+    }
+  }
+}
