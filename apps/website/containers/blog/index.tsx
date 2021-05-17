1b263515a4c45bb65ed6eb6301dbccfa045d96da
Thanakrit <Thanakrit.b@blockfint.com>
2021-05-17 17:27:47 +0700

    add key prop

--- apps/website/containers/blog/index.tsx
+++ apps/website/containers/blog/index.tsx
@@ -60,12 +60,13 @@ export const Blog: React.FC = () => {
         <HeadingText>Blog</HeadingText>
         <CategoryWrapper>
           {categoryList.map((cat) => (
-            <Category text={cat} selected={cat.toLowerCase() === category} />
+            <Category key={cat} text={cat} selected={cat.toLowerCase() === category} />
           ))}
         </CategoryWrapper>
         <BlogWrapper>
           {posts.map((post) => (
             <ThumbnailBlog
+              key={post.title}
               tagLink={post.tagLink}
               blogLink={post.blogLink}
               title={post.title}
