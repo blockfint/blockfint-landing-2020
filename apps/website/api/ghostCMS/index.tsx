79e7d6047bd3d9a1f09b7a463da51706d3e18493
jakkapat <max.jakkapat@gmail.com>
2021-05-28 16:09:31 +0700

    create preview page

--- apps/website/api/ghostCMS/index.tsx
+++ apps/website/api/ghostCMS/index.tsx
@@ -1,4 +1,5 @@
 import GhostContentAPI from '@tryghost/content-api'
+import GhostAdminAPI from '@tryghost/admin-api'
 
 // Create API instance with site credentials
 export const ghostApi = new GhostContentAPI({
@@ -7,7 +8,14 @@ export const ghostApi = new GhostContentAPI({
   version: 'v3'
 })
 
+export const adminApi = new GhostAdminAPI({
+  url: process.env.NEXT_PUBLIC_BLOG_URL,
+  key: process.env.NEXT_PUBLIC_ADMIN_API_KEY,
+  version: 'v3'
+})
+
 export {
+  getDraftPosts,
   getAllPosts,
   getSinglePost,
   getPostsByTag,
