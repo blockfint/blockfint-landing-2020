de5ede3e1d32df38be3e5ae07d963c81d0f62780
Theeraphat <akarapanwong@hotmail.com>
2022-07-05 17:03:16 +0700

    ⬆️  Nx and next js

--- apps/website/jest.config.ts
+++ apps/website/jest.config.ts
@@ -1,10 +1,11 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'website',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/website',
-};
+  coverageDirectory: '../../coverage/apps/website'
+}
