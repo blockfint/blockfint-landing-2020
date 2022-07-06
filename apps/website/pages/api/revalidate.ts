67e935d63ffbbbca09ecd11e7b126806281fc4ba
Theeraphat <akarapanwong@hotmail.com>
2022-07-06 19:01:02 +0700

    this is error revalidate

--- apps/website/pages/api/revalidate.ts
+++ apps/website/pages/api/revalidate.ts
@@ -10,9 +10,8 @@ export default async function handler(req, res) {
     await res.revalidate('/blog')
     return res.json({ revalidated: true })
   } catch (err) {
-    console.log(err)
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send('Error revalidating', err)
   }
 }
