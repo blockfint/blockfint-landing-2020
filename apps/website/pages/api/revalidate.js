f680ae0541252ba9446ed2e9ebb340f08c3c42d8
Theeraphat <akarapanwong@hotmail.com>
2022-07-08 15:02:32 +0700

    POC i18n revalidate

--- apps/website/pages/api/revalidate.js
+++ apps/website/pages/api/revalidate.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    await res.revalidate('/blog')
+    await res.revalidate('/en/blog')
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
