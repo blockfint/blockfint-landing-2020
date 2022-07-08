173dbb95835383c028e7fa607f8deedf189cb0d1
Theeraphat <akarapanwong@hotmail.com>
2022-07-08 16:21:13 +0700

    adding vercel config and revalidate 200

--- apps/website/pages/api/revalidate.js
+++ apps/website/pages/api/revalidate.js
@@ -14,6 +14,6 @@ export default async function handler(req, res) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
 
-    return res.status(500).send('Error revalidating' + err)
+    return res.status(200).send('Error revalidating' + err)
   }
 }
