360d0844e2916f169ae3cd2db717620267deaa9c
Theeraphat <akarapanwong@hotmail.com>
2022-07-08 14:30:04 +0700

    🔊  re logging method

--- apps/website/pages/api/revalidate.js
+++ apps/website/pages/api/revalidate.js
@@ -13,6 +13,6 @@ export default async function handler(req, res) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
 
-    return res.status(500).send('Error revalidating', err)
+    return res.status(500).send('Error revalidating' + err)
   }
 }
