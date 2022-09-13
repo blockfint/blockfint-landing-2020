daf99a2cc39fea98fdc434752af551c75edefed2
Theeraphat <akarapanwong@hotmail.com>
2022-09-13 14:02:18 +0700

    ✨  redirect path

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -3,6 +3,25 @@ const withNx = require('@nrwl/next/plugins/with-nx')
 const { i18n } = require('./next-i18next.config.js')
 
 module.exports = withNx({
+  async redirects() {
+    return [
+      {
+        source: '/landing',
+        destination: '/',
+        permanent: true
+      },
+      {
+        source: '/Landing',
+        destination: '/',
+        permanent: true
+      },
+      {
+        source: '/brochure',
+        destination: 'https://brochure.blockfint.com/',
+        permanent: true
+      }
+    ]
+  },
   i18n,
   images: {
     deviceSizes: [320, 420, 600, 768, 1024, 1200, 1920, 2040],
@@ -15,6 +34,7 @@ module.exports = withNx({
     })
     return config
   },
+
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
